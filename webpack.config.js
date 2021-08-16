551afc640794d02d8f4534dd1a10073298354122
Khitrov Nickolai <deliriumbred@yandex.ru>
2021-08-16 17:43:00 +0300

    Add a webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
+
+module.exports = {
+    context: path.resolve(__dirname, './src'),
+    mode: 'development',
+    entry: './index.js',
+
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'script.js'
+    },
+
+    module: {
+        rules: [
+
+            {
+                test: /\.(s[ac]ss)$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                },
+                'css-loader',
+                'sass-loader'                    
+                ],
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html')
+        }),
+
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: `./css/${filename('css')}`
+        })
+    ]
+}
